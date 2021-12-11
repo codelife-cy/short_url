af274b25bb4eb1886fa85066ec6d222b085c9c57
Cy <1150884107@qq.com>
2021-12-12 02:55:19 +0800

    数据库文件

--- sql/short.sql
+++ sql/short.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `short` (
      `id` bigint(20) NOT NULL AUTO_INCREMENT,
-     `long_url` varchar(100) DEFAULT NULL,
-     `short_url` varchar(100) DEFAULT NULL,
+     `long_url` varchar(10240) DEFAULT NULL,
+     `short_url` varchar(11) DEFAULT NULL,
      `note` varchar(100) DEFAULT NULL,
      `create_time` datetime DEFAULT NULL,
      PRIMARY KEY (`id`)
